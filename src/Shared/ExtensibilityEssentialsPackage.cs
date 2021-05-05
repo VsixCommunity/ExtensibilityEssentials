dc24d90988e7040ce7e52791019118b2dad28de0
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-05-05 11:36:11 -0700

    Upgraded to latest toolkit package

--- src/Shared/ExtensibilityEssentialsPackage.cs
+++ src/Shared/ExtensibilityEssentialsPackage.cs
@@ -14,107 +14,48 @@ namespace ExtensibilityEssentials
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
     [ProvideAutoLoad("07ce51b0-5439-4c81-bbc6-dc629f343357", flags: PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideUIContextRule("07ce51b0-5439-4c81-bbc6-dc629f343357",
-        name: "VSIX project",
-        expression: "vsix",
-        termNames: new[] { "vsix" },
-        termValues: new[] { "SolutionHasProjectFlavor:{82b43b9b-a64c-4715-b499-d71e9ca2bd60}" })]
+        name: "autoload",
+        expression: "vsix & building",
+        termNames: new[] { "vsix", "building" },
+        termValues: new[] { "SolutionHasProjectFlavor:" + ProjectTypes.EXTENSIBILITY, VSConstants.UICONTEXT.SolutionBuilding_string })]
     public sealed partial class ExtensibilityEssentialsPackage : ToolkitPackage
     {
-        private static readonly Guid _vsixProjectSubType = new("82b43b9b-a64c-4715-b499-d71e9ca2bd60");
-
-#nullable disable
-        private IVsSolution _solution;
-#nullable restore
-
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
-            _solution = await VS.Solution.GetSolutionAsync();
+            VS.Events!.SolutionEvents!.Opened += OnSolutionOpened;
+            VS.Events!.SolutionEvents!.ProjectAdded += ApplyBuildPropertyToProject;
 
-            if (_solution is not null)
+            IVsSolution _solution = await VS.Solution.GetSolutionAsync();
+
+            if (_solution.IsOpen())
             {
-                // Listen for solutions being opened and new projects being added to existing
-                // solutions so that we can add a build property to each VSIX project.
-                SolutionEvents events = VS.Events.SolutionEvents;
-
-                if (events is not null)
-                {
-                    events.Opened += OnSolutionOpened;
-                    events.ProjectAdded += OnProjectAdded;
-                }
-
-                // If a solution is already open, then act as though it has just been opened.
-                // TODO: In newer versions of the toolkit, call _solution.IsOpen() instead;
-                if (ErrorHandler.Succeeded(_solution.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out var value)))
-                {
-                    if (value is bool isOpen && isOpen)
-                    {
-                        OnSolutionOpened();
-                    }
-                }
+                OnSolutionOpened();
             }
         }
 
         private void OnSolutionOpened()
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            IEnumerable<IVsHierarchy> hierarchies = _solution.GetAllProjectHierarchys();
-
-            foreach (IVsHierarchy hierarchy in hierarchies)
+            JoinableTaskFactory.RunAsync(async delegate
             {
-                if (IsVsixProject(hierarchy))
+                await JoinableTaskFactory.SwitchToMainThreadAsync(DisposalToken);
+                IEnumerable<Project> projects = await VS.Solution.GetAllProjectsInSolutionAsync();
+
+                foreach (Project project in projects)
                 {
-                    ApplyBuildPropertyToProject(hierarchy);
+                    ApplyBuildPropertyToProject(project);
                 }
-            }
+            });
         }
 
-        private void OnProjectAdded(Project project)
+        private void ApplyBuildPropertyToProject(Project project)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            if (ErrorHandler.Succeeded(_solution.GetProjectOfUniqueName(project.UniqueName, out IVsHierarchy hierarchy)))
+            if (project.IsKind(ProjectTypes.EXTENSIBILITY))
             {
-                if (IsVsixProject(hierarchy))
-                {
-                    ApplyBuildPropertyToProject(hierarchy);
-                }
-            }
-        }
-
-        // TODO: Replace with project.IsKind(VSIXGuid) in newer versions of the Tookit
-        private bool IsVsixProject(IVsHierarchy hierarchy)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            if (hierarchy is IVsAggregatableProject aggregatable)
-            {
-                if (ErrorHandler.Succeeded(aggregatable.GetAggregateProjectTypeGuids(out var types)))
-                {
-                    foreach (var type in types.Split(';'))
-                    {
-                        if (Guid.TryParse(type, out Guid identifier) && _vsixProjectSubType.Equals(identifier))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        // TODO: Use Project.TrySetBuildProperty in newer versions of the Toolkit
-        private void ApplyBuildPropertyToProject(IVsHierarchy hierarchy)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            if (hierarchy is IVsBuildPropertyStorage storage)
-            {
-                // Store the build property in the user file instead of the project
-                // file, because we don't want to affect the real project file.
-                storage.SetPropertyValue("ExtensibilityEssentialsInstalled", "", (uint)_PersistStorageType.PST_USER_FILE, "true");
+                project.TrySetBuildPropertyAsync("ExtensibilityEssentialsInstalled", "true").ForgetAndLogOnFailure();
             }
         }
     }
